55e3a27437b8b4870fdba587cf10597dfeee47d4
unknown <https://github.com/AdaDev11/resumedrafts.git>
2026-03-11 04:18:29 +0500

    fix nextjs params

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,12 @@
 "use client";
+
 import React, { useState } from "react";
 import { Box } from "@mui/material";
 import Header from "../../components/header";
 import AllProductsPage from "../../components/products/page";
 import Footer from "../../components/footer";
 
-type Params = {
-    products: string;
-};
-
-export default function Page({ params }: { params: Params }) {
-    const { products } = params;
-
+export default function Page() {
     const [searchValue, setSearchValue] = useState("");
     const [category, setCategory] = useState("all");
     const [sort, setSort] = useState("asc");
